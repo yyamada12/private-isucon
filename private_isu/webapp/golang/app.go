55da8c261180745b2de8a5e7656e30db093fa03f
yyamada12 <12yacropolisy@gmail.com>
2023-07-15 18:55:42 +0900

    [59839] limit 20 at getPosts

--- private_isu/webapp/golang/app.go
+++ private_isu/webapp/golang/app.go
@@ -731,7 +731,7 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	results := []Post{}
-	err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC", t.Format(ISO8601Format))
+	err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC LIMIT 20", t.Format(ISO8601Format))
 	if err != nil {
 		log.Print(err)
 		return
