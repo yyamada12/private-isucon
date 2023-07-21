d0e979226fc3cd0e88bef2282dcd676d54eff338
yyamada12 <12yacropolisy@gmail.com>
2023-07-21 23:38:19 +0900

    [111933] add index users(account_name)

--- private_isu/webapp/index.sql
+++ private_isu/webapp/index.sql
@@ -1,2 +1,3 @@
 alter table comments add index idx_post_id_created_at(post_id, created_at);
 alter table posts add index idx_created_at(created_at);
+alter table users add index idx_account_name(account_name);
