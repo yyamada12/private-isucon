648c73c8343ba946f25f10da0396942c3c361d26
yyamada12 <12yacropolisy@gmail.com>
2023-07-26 18:09:13 +0900

    add index for count comments by user_id

--- private_isu/webapp/index.sql
+++ private_isu/webapp/index.sql
@@ -1,3 +1,4 @@
 alter table comments add index idx_post_id_created_at(post_id, created_at);
 alter table posts add index idx_created_at(created_at);
 alter table users add index idx_account_name(account_name);
+alter table comments add index idx_user_id(user_id);
