3d031083f29cb153647da910a3d90a414e120e1e
yyamada12 <12yacropolisy@gmail.com>
2023-07-26 13:26:32 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,11 @@ echo "Started deploying."
 # rotate logs
 function rotate_log () {
   if sudo [ -e $1 ]; then
-    sudo mv $1 ${1%.*}_$(date +"%Y%m%d%H%M%S").${1##*.}
+    sudo mv $1 ${1%.*}_bak.${1##*.}
   fi
 }
 rotate_log /var/log/nginx/access.log
+rotate_log /var/log/nginx/error.log
 rotate_log /var/log/mysql/slow.log
 rotate_log ~/pprof/pprof.png
 
